537c64d9098d5db5b7f662cf914309819cc42b43
Piotr Flak <88963612+PFlak@users.noreply.github.com>
2025-11-18 19:59:59 +0100

    Basic Backend (#8)

---
+++ src/backend/models/alerts.py
@@ -0,0 +1,21 @@
+from typing import Literal, Optional
+from pydantic import BaseModel, Field
+
+from .alert_parameters import AlertParameters
+
+
+class Alert(BaseModel):
+    id: int
+    boat_id: int
+    timestamp: float
+    alert_type: Literal['dangerr', 'weather', 'other']
+    severity: int = Field(0, ge=0, le=3)
+    description: str
+    parameters: list[AlertParameters] = Field([])
+    latitude: Optional[float] = Field(None)
+    longitude: Optional[float] = Field(None)
+    resolved: Optional[bool] = Field(False)
+
+
+class Alerts(BaseModel):
+    array: list[Alert]
